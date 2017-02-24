fd9889d7a13176ca62e8b142cfb1784f16e10a73
Jackson Curtis <jcurtis210@gmail.com>
2017-02-23 19:34:55 -0700

    in class 7

---
+++ InClass7/app.js
@@ -0,0 +1,27 @@
+angular.module('News', [])
+  .factory('postFactory', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+  }])
+ .controller('MainCtrl', [
+    '$scope',
+    'postFactory',
+    function($scope, postFactory){
+      $scope.posts = postFactory.posts;
+	
+	
+	$scope.addPost = function() {
+    $scope.posts.push({title:$scope.formContent,upvotes:0});
+    $scope.formContent='';
+	
+  };
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+  
+  
+  
+    }
+    ]);
